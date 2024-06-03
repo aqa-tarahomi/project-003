d32a6e7dedabfadec0c9a78ce0aa881a46ffe98e
mohammad-tarah <aqatarahomi8@gmail.com>
2024-06-03 23:38:13 +0330

    ok

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,30 +1,33 @@
-const theme_icon=document.getElementById("theme-switch-icon")
-function change(){
-    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-        document.documentElement.setAttribute('data-bs-theme' , 'light')
-        theme_icon.setAttribute("class","fa-solid fa-sun")
-    }
-
-    else{
-        document.documentElement.setAttribute('data-bs-theme' ,'dark')
-        theme_icon.setAttribute("class","bi bi-moon-stars-fill text-info")
-    }
-}
-const pain= document.getElementById("pain");
-  function myFunction(x) {
-    if (x.matches) {
-            pain.setAttribute("class","btn bi bi-person-fill")
-            pain.innerHTML=""
-          } else {
-              pain.setAttribute("class","btn me-5")
-               pain.innerHTML="Login"
-           }
+const theme_icon = document.getElementById("theme-switch-icon")
+const intro = document.getElementById("introduction-header")
+function change() {
+  if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+    document.documentElement.setAttribute('data-bs-theme', 'light')
+    theme_icon.setAttribute("class", "bi bi-sun-fill")
   }
-  
-  var x = window.matchMedia("(max-width: 768px)")
-  
+
+  else {
+    document.documentElement.setAttribute('data-bs-theme', 'dark')
+    theme_icon.setAttribute("class", "bi bi-moon-stars-fill text-info")
+  }
+}
+
+const pain = document.getElementById("pain");
+function myFunction(x) {
+  if (x.matches) {
+    pain.setAttribute("class", "btn bi bi-person-fill")
+    intro.classList.replace("display-1","display-2")
+    pain.innerHTML = ""
+  } else {
+    pain.setAttribute("class", "btn me-5")
+    pain.innerHTML = "Login"
+  }
+}
+
+var x = window.matchMedia("(max-width: 768px)")
+
+myFunction(x);
+
+x.addEventListener("change", function () {
   myFunction(x);
-  
-  x.addEventListener("change", function() {
-    myFunction(x);
-  });
+});
